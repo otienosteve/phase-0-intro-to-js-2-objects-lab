880344d24732dc00739ec005bfa512adf412d8c8
otienosteve <steveotieno701@gmail.com>
2022-04-14 07:25:55 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+let employee = {name:"Sam",
+streetAddress:"Kano"}
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    let placehold={...employee};
+    placehold.streetAddress=value;
+    return placehold;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+employee.streetAddress=value;
+return employee;
+}
+function deleteFromEmployeeByKey(employee, key){
+var placehold={...employee}
+delete placehold[key];
+return placehold;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+delete employee[key];
+return employee;
+}
